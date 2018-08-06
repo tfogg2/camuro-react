84f1c3921c3cd0b1baf6a54840319a04066c8f32
Thomas Fogg <tfogg2@gmail.com>
2018-08-06 11:40:35 -0400

    preliminary cart styling

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,20 +1,41 @@
 import React, {PropTypes} from 'react'
+import { Link } from 'react-router-dom'
 
 const Cart = props => {
-  return (
-    <div className="cart">
-      <div className="cart-content">
-        {props.cartProducts.map((product, index) => (
-          <div className="product-title">
-            <a className="remove-product" onClick={ () => props.removeProduct(index)}>✖</a>
-            <h3>{product.title}</h3>
-            <p>{product.description}</p>
-            <span>${product.price}</span>
-          </div>
-        ))}
+  const products = props.cartProducts
+  if(products.length > 0 ){
+    return (
+      <div className="cart">
+        <div className="cart-content">
+          {props.cartProducts.map((product, index) => (
+            <div className="cart-product">
+              <div className="cart-product-title">
+                <h3>{product.title}</h3>
+              </div>
+              <div className="cart-product-description">
+                <p>{product.description}</p>
+              </div>
+              <div className="remove-product">
+                <a className="remove-product" onClick={ () => props.removeProduct(index)}>✖</a>
+              </div>
+              <div className="cart-product-price">
+                <span>${product.price}</span>
+              </div>
+            </div>
+          ))}
+          <Link to="/products">Continue Shopping</Link>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+  else {
+    return (
+      <div className="cart">
+        <h3><Link to="/products">Add some products!</Link></h3>
+      </div>
+    )
+  }
+
 }
 
 export default Cart;
