8194c8aea2c63e17e54e1a33a14262fe4892916d
Thomas Fogg <tfogg2@gmail.com>
2018-08-08 11:52:06 -0400

    react form

--- src/Components/Offers/OfferForm.js
+++ src/Components/Offers/OfferForm.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import ReactContactForm from 'react-mail-form';
 
 class OfferForm extends Component {
   render(){
     return(
       <div className="content">
-
+        <ReactContactForm to="address@gmail.com" />
       </div>
     )
   }
