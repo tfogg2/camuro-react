0c3abb97161b2ed63372c539473c5524741db5e2
tfogg2 <foggts@g.cofc.edu>
2018-08-12 09:31:52 -0400

    home

---
+++ src/Components/Credit.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+
+class Credit extends Component {
+  render(){
+    return(
+      <ul className="credit">
+        <li>Photo by <a href="https://unsplash.com/photos/vn5MU5VwdjQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" >FilterGrade</a></li>
+        <li>Photo by <a href="https://unsplash.com/photos/LaxqfiF6yp8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Ariel Domenden</a></li>
+        <li>Photo by <a href="https://unsplash.com/photos/4fydl6bJT8k?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank"> Chad Dalke</a></li>
+        <li>Photo by <a href="https://unsplash.com/photos/-3savqescr8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Hanny Naibaho</a></li>
+        <li>Photo by <a href="https://unsplash.com/photos/99Ty_Vme3wA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Joseph Chan</a></li>
+        <li>Photo by <a href="https://unsplash.com/photos/_T3upUiCqEE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Jesus Santos</a></li>
+        <li>Photo by <a href="https://unsplash.com/photos/1z1F5Qc30Bs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Mia Domenico</a></li>
+        <li>Photo by <a href="https://unsplash.com/photos/2TCy8pqFXsE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Mpho Mojapelo</a></li>
+        <li>Photo by <a href="https://unsplash.com/photos/q2xUk_LEFOw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Luo ping</a></li>
+        <li>Photo by <a href="" target="_blank"></a></li>
+        <li>Photo by <a href="" target="_blank"></a></li>
+        <li>Photo by <a href="" target="_blank"></a></li>
+      </ul>
+    )
+  }
+}
+
+export default Credit;
--- src/Components/Offers/OfferForm.js
+++ src/Components/Offers/OfferForm.js
@@ -1,18 +1,85 @@
 import React, {Component} from 'react';
-import ReactContactForm from 'react-mail-form';
+import ReactContactForm from 'react-mail-form'
+import axios from 'axios'
+import SelectUSState from 'react-select-us-states'
 
 class OfferForm extends Component {
+  state = {
+    active: false
+  }
+
+
+  handleSubmit(e){
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const state = document.getElementById('state').value;
+    const modelName = document.getElementById('modelName').value;
+    const condition = document.getElementById('condition').value;
+    axios({
+        method: "POST",
+        url:"http://localhost:3002/send",
+        data: {
+            name: name,
+            email: email,
+            condition: condition
+        }
+    }).then((response)=>{
+        if (response.data.msg === 'success'){
+            alert("Message Sent.");
+            this.resetForm()
+        }else if(response.data.msg === 'fail'){
+            alert("Message failed to send.")
+        }
+    })
+  }
+
+  inputActive() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  };
+
+
+  resetForm(){
+    document.getElementById('contact-form').reset();
+  }
+
   render(){
+    const inputClass = this.state.active ? 'active' : ''
     return(
       <div className="sell-content">
         <div className="sell-form-content">
-          <h1>Tell us about your product</h1>
-          <ul>
-            <label>Things to include:</label>
-            <li>Breif description of cosmetics</li>
-            <li>Breif description of cosmetics</li>
-          </ul>
-          <ReactContactForm to="address@gmail.com" className="sell-form" titlePlaceholder="Canon Rebel..." buttonText="Send" contentsPlaceholder="Description..."/>
+          <h1>Offer Form</h1>
+          <form className="sell-form" id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+            <div className="form-group">
+              <label>NAME</label>
+              <input type="name" className={inputClass} id="name" aria-describedby="customerName" onClick={this.inputActive.bind(this)}/>
+            </div>
+            <div className="form-group">
+              <label>EMAIL</label>
+              <input type="email" className={inputClass} id="email" aria-describedby="emailHelp" onClick={this.inputActive.bind(this)}/>
+            </div>
+            <div className="form-group">
+              <label>STATE</label>
+              <SelectUSState id="state" className="myClassName"/>
+            </div>
+            <div className="form-group">
+              <label>MODEL NAME OR DESCRIPTION</label>
+              <input type="text" className={inputClass} id="modelName" onClick={this.inputActive.bind(this)}/>
+            </div>
+            <div className="form-group">
+              <label>ITEM CONDITION</label>
+              <select id="condition" placeholder="Select item description">
+                <option>Like New</option>
+                <option>Excellent</option>
+                <option>Good</option>
+                <option>Well Used</option>
+                <option>Heavily Used</option>
+                <option>Faulty</option>
+              </select>
+            </div>
+            <button type="submit" className="btn btn-primary">Get A Quote Now</button>
+          </form>
         </div>
       </div>
     )
