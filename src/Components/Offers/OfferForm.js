381c278f94fbeeeae35dc5e1fc1790cdb4b60d98
Thomas Fogg <tfogg2@gmail.com>
2018-08-16 10:33:40 -0400

    validations

--- src/Components/Offers/OfferForm.js
+++ src/Components/Offers/OfferForm.js
@@ -5,17 +5,31 @@ import SelectUSState from 'react-select-us-states'
 
 class OfferForm extends Component {
   state = {
-    active: false
+    activeField: null,
+    invalidFields: []
   }
 
-  handleSubmit(e){
+  handleSubmit = (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const state = document.getElementById('state').value;
-    const model = document.getElementById('model').value;
-    const condition = document.getElementById('condition').value;
-    const price = document.getElementById('price').value;
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const state = document.getElementById('state').value
+    const model = document.getElementById('model').value
+    const condition = document.getElementById('condition').value
+    const price = document.getElementById('price').value
+
+    const invalidFields = []
+    if (!email || email.length < 1) { invalidFields.push('email') }
+    if (!name || name.length < 1) { invalidFields.push('name') }
+    if (!model || model.length < 1) { invalidFields.push('model') }
+    if (!price || price.length < 1) { invalidFields.push('price') }
+
+    this.setState({
+      invalidFields: invalidFields
+    })
+
+    if (invalidFields.length > 0) { return false }
+
     const message = `state: ${state} \n model: ${model} \n condition: ${condition} \n price: ${price}`
     axios({
         method: "POST",
@@ -36,10 +50,25 @@ class OfferForm extends Component {
     })
   }
 
-  inputActive() {
-    const currentState = this.state.active;
-    this.setState({ active: !currentState });
-  };
+  inputActive = (field) => {
+    const currentState = this.state.active
+    this.setState({ activeField: field})
+  }
+
+  inputClass = (field) => {
+    let fieldClasses = []
+    // Check to see if field is currently active
+    if (this.state.activeField === field) {
+      fieldClasses.push('active')
+    }
+    // Check if field is in array of invalid fields
+    if (this.state.invalidFields.indexOf(field) !== -1) {
+      fieldClasses.push('error')
+    }
+
+    return fieldClasses.join(' ')
+  }
+
 
 
   resetForm(){
@@ -47,19 +76,18 @@ class OfferForm extends Component {
   }
 
   render(){
-    const inputClass = this.state.active ? 'active' : ''
     return(
       <div className="sell-content">
         <div className="sell-form-content">
           <h3>Get An Offer</h3>
-          <form className="sell-form" id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+          <form className="sell-form" id="contact-form" onSubmit={this.handleSubmit} method="POST">
             <div className="form-group">
               <label for="name">NAME</label>
-              <input type="name" className={inputClass} id="name" aria-describedby="customerName" onClick={this.inputActive.bind(this)}/>
+              <input type="name" className={this.inputClass('name')} id="name" aria-describedby="customerName" onClick={() => this.inputActive("name")}/>
             </div>
             <div className="form-group">
               <label for="email">EMAIL</label>
-              <input type="email" className={inputClass} id="email" aria-describedby="emailHelp" onClick={this.inputActive.bind(this)}/>
+              <input type="email" className={this.inputClass('email')} id="email" aria-describedby="emailHelp" onClick={() => this.inputActive("email")}/>
             </div>
             <div className="form-group">
               <label for="state">STATE</label>
@@ -68,11 +96,11 @@ class OfferForm extends Component {
             <div className="form-group item price">
               <label for="price">ASKING PRICE</label>
               <span>$</span>
-              <input type="number" className={inputClass} id="price" onClick={this.inputActive.bind(this)}/>
+              <input type="number" className={this.inputClass('price')} id="price" onClick={() => this.inputActive("price")}/>
             </div>
             <div className="form-group item">
               <label for="modelName">MODEL</label>
-              <input type="text" className={inputClass} id="model" onClick={this.inputActive.bind(this)}/>
+              <input type="text" className={this.inputClass('model')} id="model" onClick={() => this.inputActive("model")}/>
             </div>
             <div className="form-group item">
               <label>ITEM CONDITION</label>
